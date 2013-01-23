b8b452a1944504c8acaa30fc8677810bd5ba1665
jzhuan5 <jin.can.zhuang@intel.com>
2013-01-23 14:06:40 -0500

    [PORT FROM MAIN] start MtpServer if it's teminated.
    BZ: 72629
    
    when starting MtpService, if MtpServer is not NULL but its thread is
    already terminated, we should start the thread again.
    
    Change-Id: I6de2946f62cd28d57b9808b727ce847540b7c850
    Orig-Change-Id: Id3d4ba7ce78dae725ca0242091eba85e486c40da
    Signed-off-by: jzhuan5 <jin.can.zhuang@intel.com>
    Reviewed-on: http://android.intel.com:8080/123484
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: Wang, Ran <ran.wang@intel.com>
    Reviewed-by: Wang, Yu Y <yu.y.wang@intel.com>
    Reviewed-by: Huang, Bo B <bo.b.huang@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- src/com/android/providers/media/MtpService.java
+++ src/com/android/providers/media/MtpService.java
@@ -183,7 +183,10 @@ public class MtpService extends Service {
      */
     private void manageServiceLocked() {
         final boolean isCurrentUser = UserHandle.myUserId() == ActivityManager.getCurrentUser();
-        if (mServer == null && isCurrentUser) {
+        if ((mServer == null || mServer.getState() == Thread.State.TERMINATED) && isCurrentUser) {
+            if (mServer != null)
+                Log.d(TAG, "MTP Server is not running");
+
             Log.d(TAG, "starting MTP server in " + (mPtpMode ? "PTP mode" : "MTP mode"));
             mServer = new MtpServer(mDatabase, mPtpMode);
             if (!mMtpDisabled) {
