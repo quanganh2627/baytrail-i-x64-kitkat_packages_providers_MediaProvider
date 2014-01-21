b215b8081f4fa4c50886fafd5259e809cd771449
Bo Huang <bo.b.huang@intel.com>
2014-01-21 15:10:12 +0800

    Revert "Mtpservice: Correct KeyguardLocked only case if reboot DUT."
    BZ: 165717
    
    This revert commit 69134350997619387c676cd8651fa36f4a46bb8c.
    One regression cause by the change.
    
    Change-Id: Id5e696a04ebe0aa3bbfd081fe1a758979c36b99d
    Category: aosp improvement
    Domain: USB-Mtp-ptp
    Origin: internal
    Upstream-Candidate: no, proprietary
    Signed-off-by: Bo Huang <bo.b.huang@intel.com>

--- src/com/android/providers/media/MtpService.java
+++ src/com/android/providers/media/MtpService.java
@@ -231,7 +231,7 @@ public class MtpService extends Service {
         final boolean isCurrentUser = UserHandle.myUserId() == ActivityManager.getCurrentUser();
         final KeyguardManager keyguardManager = (KeyguardManager) getSystemService(
                 Context.KEYGUARD_SERVICE);
-        mMtpDisabled = (keyguardManager.isKeyguardLocked() || keyguardManager.isKeyguardSecure())
+        mMtpDisabled = (keyguardManager.isKeyguardLocked() && keyguardManager.isKeyguardSecure())
                 || !isCurrentUser;
         if (LOGD) {
             Log.d(TAG, "updating state; isCurrentUser=" + isCurrentUser + ", mMtpLocked="
